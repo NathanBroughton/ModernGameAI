887e8495bbf94556bfc1c8f9587777605691aa60
vikkyb <victorbatenburg@hotmail.com>
2022-04-04 18:15:08 +0200

    More confident it works

--- mcts_bot_v2.py
+++ mcts_bot_v2.py
@@ -108,47 +108,110 @@ class MCTSbot(botbowl.Agent):
     def __init__(self, name, seed=None):
         super().__init__(name)
         self.my_team = None
+        self.coin = "heads"
+        self.KR = "kick"
         self.rnd = np.random.RandomState(seed)
+        self.m = 1
+        self.formation = False
+        self.off_formation = [
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "x", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "x"],
+            ["-", "-", "-", "p", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "s"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "x", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"]
+        ]
+
+        self.def_formation = [
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "S", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "0"],
+            ["-", "-", "-", "-", "x", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "0"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "S", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"]
+        ]
+        self.off_formation = botbowl.Formation("Wedge offense", self.off_formation)
+        self.def_formation = botbowl.Formation("Zone defense", self.def_formation)
+        self.setup_actions = []
 
     def new_game(self, game, team):
         self.my_team = team
 
     def act(self, game):
         # print("Time to take an action")
-        # TODO: implement setup
 
-        # Time MCTS
-        time_start = time.time()
-        game_copy = copy.deepcopy(game)
-        game_copy.enable_forward_model()
-        game_copy.home_agent.human = True
-        game_copy.away_agent.human = True
+        ##Scripted coin flip
+        # Why is this needed? can't you just set which team starts. Haven't seen actiontype.Heads coming up
+        if (game.state.available_actions[0].action_type == botbowl.ActionType.HEADS):
+            if(self.coin == "heads"):
+                action = botbowl.Action(game.state.available_actions[0].action_type)
+            else:
+                action = botbowl.Action(game.state.available_actions[1].action_type)
 
-        # Create root node
-        root_node = MCTSNode()
-        root_step = game_copy.get_step()
+        ##Scripted kick/receive
+        # Kick can now be selected before a team is formed
+        elif(game.state.available_actions[0].action_type == botbowl.ActionType.KICK):
+            if(self.KR == "kick"):
+                action = botbowl.Action(game.state.available_actions[0].action_type)
+            else:
+                action = botbowl.Action(game.state.available_actions[1].action_type)
 
-        # print("Simulating {0} games".format(n_simulations))
-        for i in range(n_simulations):
-            # print("Simulation", i)
-            # Find a leaf node
-            leaf_node, game_copy = self.traverse(root_node, game_copy)
-            # Rollout until depth reached or terminal node
-            game_copy = self.rollout(game_copy)
-            # Evaluate rollout result
-            score = self.evaluate(game_copy)
-            # Backprop score
-            leaf_node.backpropagate(score)
-            # Reset to root node
-            game_copy.revert(root_step)
+        ##Scripted Formation, does not work yet!!!!
+        # Place a player according to the formation when your team does't have 5 players in a position
+        elif(game.state.available_actions[0].action_type == botbowl.ActionType.PLACE_PLAYER):
+            # if [player.position != None for player in self.my_team.players].count(True) != 5:
+            if (self.formation == False) or (self.formation == True and len(self.setup_actions) != 0):
+                action = self.setup(game)
+            elif [player.position != None for player in self.my_team.players].count(True) == 5:
 
-        # Select best action
-        best_action = root_node.best_action(c_val=0.)
+            # else:
+                action = botbowl.Action(game.state.available_actions[1].action_type)
 
-        time_end = time.time()
-        print("Found best action:", best_action,
-              "in {0} seconds".format(time_end - time_start))
-        return best_action
+        elif(game.state.available_actions[0].action_type == botbowl.ActionType.PLACE_BALL):
+            position = self.rnd.choice(game.state.available_actions[0].positions)
+            action = botbowl.Action(game.state.available_actions[0].action_type, position=position)
+
+        ##Stuur deepcopy naar MCTS class om beste actie te kiezen
+        else:
+            # Time MCTS
+            time_start = time.time()
+            game_copy = copy.deepcopy(game)
+            game_copy.enable_forward_model()
+            game_copy.home_agent.human = True
+            game_copy.away_agent.human = True
+
+            # Create root node
+            root_node = MCTSNode()
+            root_step = game_copy.get_step()
+
+            # print("Simulating {0} games".format(n_simulations))
+            for i in range(n_simulations):
+                # print("Simulation", i)
+                # Find a leaf node
+                leaf_node, game_copy = self.traverse(root_node, game_copy)
+                # Rollout until depth reached or terminal node
+                game_copy = self.rollout(game_copy)
+                # Evaluate rollout result
+                score = self.evaluate(game_copy)
+                # Backprop score
+                leaf_node.backpropagate(score)
+                # Reset to root node
+                game_copy.revert(root_step)
+
+            # Select best action
+            best_action = root_node.best_action(c_val=0.)
+
+            time_end = time.time()
+            print("Found best action:", best_action,
+                "in {0} seconds".format(time_end - time_start))
+            action = best_action
+        return action
 
     def traverse(self, root_node, game_copy):
         # This is spaghetti, but I tried to document it as well as possible
@@ -267,6 +330,29 @@ class MCTSbot(botbowl.Agent):
             distance2endzone = abs(target_x - x_ball)
             score -= distance2endzone
         return score
+
+    def setup(self, game):
+        self.my_team = game.get_team_by_id(self.my_team.team_id)
+        self.opp_team = game.get_opp_team(self.my_team)
+
+        # Get the first action if a formation and their actions have been set
+        if self.setup_actions:
+            action = self.setup_actions.pop(0)
+
+        # If no formation was set, create the actions now and return the first action
+        elif not self.setup_actions and self.formation == False:
+            if game.get_receiving_team() == self.my_team:
+                self.setup_actions = self.off_formation.actions(game, self.my_team)
+                self.setup_actions.append(botbowl.Action(botbowl.ActionType.END_SETUP))
+                self.formation = True
+                action = self.setup_actions.pop(0)
+
+            else:
+                self.setup_actions = self.def_formation.actions(game, self.my_team)
+                self.setup_actions.append(botbowl.Action(botbowl.ActionType.END_SETUP))
+                self.formation = True
+                action = self.setup_actions.pop(0)
+        return(action)
 
     def end_game(self, game):
         pass
